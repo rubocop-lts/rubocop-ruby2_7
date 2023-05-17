a07a723086e94f2f427ecd18a452435041809a0d
Peter Boling <peter.boling@gmail.com>
2023-05-17 20:23:28 +0700

    ➕ standard-performance, standard-custom, standard-rubocop-lts, betterlint, standard-rails, version_gem

--- lib/rubocop/ruby2_7.rb
+++ lib/rubocop/ruby2_7.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
+# external libs
+require "version_gem"
+
+# this gem
 require_relative "ruby2_7/version"
 # :nocov:
 require_relative "ruby2_7/railtie" if defined?(Rails::Railtie)
@@ -12,4 +16,8 @@ module Rubocop
       load "rubocop/ruby2_7/tasks.rake"
     end
   end
+end
+
+Rubocop::Ruby27::Version.class_eval do
+  extend VersionGem::Basic
 end
