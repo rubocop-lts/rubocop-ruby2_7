0ecdf4c76e0daddf3c906cdef6879f0645ebfb04
Peter H. Boling <peter.boling@gmail.com>
2026-08-03 18:02:04 -0600

    🎨 Template bootstrap by kettle-jem v7.1.0

--- spec/rubocop/ruby2_7/version_spec.rb
+++ spec/rubocop/ruby2_7/version_spec.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require "anonymous_loader"
+require "rubocop/ruby2_7"
 RSpec.describe Rubocop::Ruby27::Version do
   it_behaves_like "a Version module", described_class
 
